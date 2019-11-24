d96343a53cbba89ff41a7153dea92da8729ba775
Chase Thrower <chasethrower@gmail.com>
2019-11-24 08:05:11 -0600

    added static settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -140,3 +140,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
