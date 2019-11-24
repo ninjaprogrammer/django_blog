8a2183498aa4ef2b81d6571ecd4b5bf24219ae5f
Chase Thrower <chasethrower@gmail.com>
2019-11-24 08:12:48 -0600

    added heroku to allowed host

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(kw0#xw2d^g(ex!h=6_!mtg_#m*76(t*yoq9daj@nm9eca)gnu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://pydjangblog.herokuapp.com/']
 
 
 # Application definition
